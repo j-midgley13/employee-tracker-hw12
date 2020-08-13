e3e6e1be724d86cb6a91099cf2e580f31169111a
Jameson Midgley <jameson.midgley@gmail.com>
2020-08-13 15:30:41 -0600

    added console.table. First inquirer responses

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // boilerplate from assignments
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-
+const consoleTable = require("console.table");
 // create the connection information for the sql database
-const connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -13,13 +13,54 @@ const connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: "Ben13folds!",
   database: "employeeTracker_db"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
+    console.log("I am in!")
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
-}); 
+});
+
+// More choices once employee ones work.
+// "View Departments", "Add Department", "View Roles", "Add Role", 
+
+function start() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "startMenu",
+            message: "Welcome to the employee tracker! Choose from the choices below.",
+            choices: ["View Employees", "Add Employee", "Update Employee Role", "Exit"]
+        },
+        ).then(response => {
+            switch(response.startMenu) {
+
+                case "View Employees":
+                    console.log("VE success")
+                    // viewEmployees();
+                    start();
+                    break;
+
+                case "Add Employee":
+                    console.log("AE success")
+                    // addEmployee();
+                    start();
+                    break;
+
+                case "Update Employee Role":
+                    console.log("UER success")
+                    // updateEmployee();
+                    start();
+                    break;
+
+                case "Exit":
+                    console.log("exit success")
+                    return connection.end();
+
+            };
+        })
+};
